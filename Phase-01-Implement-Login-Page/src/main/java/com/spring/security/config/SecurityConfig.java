50d65ddeaf8b9da4871fc18528583dd2fe907672
tranquanglong999 <tranquanglong999a1@gmail.com>
2023-12-10 09:05:44 +0700

    phase-01-implement-login-page

---
+++ Phase-01-Implement-Login-Page/src/main/java/com/spring/security/config/SecurityConfig.java
@@ -0,0 +1,44 @@
+package com.spring.security.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Bean
+    public UserDetailsService userDetailsService(){
+        var manager = new InMemoryUserDetailsManager();
+        var user1 = User.withUsername("admin").password(passwordEncoder().encode("abc123")).roles("ADMIN").build();
+        manager.createUser(user1);
+        return manager;
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests()
+                .mvcMatchers("/login").permitAll()
+                .mvcMatchers("/login-error").permitAll()
+                .mvcMatchers("/perform_login").permitAll()
+                .anyRequest().authenticated()
+                .and()
+                .formLogin()
+                .loginPage("/login")
+                .loginProcessingUrl("/perform_login")
+                .failureUrl("/login-error")
+                .defaultSuccessUrl("/home", true);
+    }
+}
