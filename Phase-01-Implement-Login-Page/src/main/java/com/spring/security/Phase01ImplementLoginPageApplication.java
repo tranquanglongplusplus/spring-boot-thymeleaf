dc9068f8aea6aeacc8eadfd0cd78e5d1c6d97fca
tranquanglong999 <tranquanglong999a1@gmail.com>
2023-12-11 02:01:49 +0700

    refactor-code

--- Phase-01-Implement-Login-Page/src/main/java/com/spring/security/Phase01ImplementLoginPageApplication.java
+++ Phase-01-Implement-Login-Page/src/main/java/com/spring/security/Phase01ImplementLoginPageApplication.java
@@ -2,14 +2,11 @@ package com.spring.security;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @SpringBootApplication
 public class Phase01ImplementLoginPageApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(Phase01ImplementLoginPageApplication.class, args);
 	}
 
 }
-K
